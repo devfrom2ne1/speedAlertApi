9708cd72927a2485e6212f02a363416d4c4bf9fa
devfrom2ne1 <124670711+devfrom2ne1@users.noreply.github.com>
2025-01-12 15:13:29 +0900

    create springboot3 project

---
+++ src/main/java/org/dev2ne1/speedalertapi/SpeedAlertApiApplication.java
@@ -0,0 +1,13 @@
+package org.dev2ne1.speedalertapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedAlertApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeedAlertApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dev2ne1/speedalertapi/SpeedAlertApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev2ne1.speedalertapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedAlertApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
