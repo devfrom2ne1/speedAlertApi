57aa57f6b8a8563397d567d853fc0f203927e11d
devfrom2ne1 <124670711+devfrom2ne1@users.noreply.github.com>
2025-01-14 15:08:25 +0900

    Parameter add(latitude, longitude)

--- src/main/java/org/dev2ne1/speedalertapi/controller/TrafficCameraController.java
+++ src/main/java/org/dev2ne1/speedalertapi/controller/TrafficCameraController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -17,9 +18,9 @@ public class TrafficCameraController {
     private final TrafficCameraService trafficCameraService;
 
     @GetMapping("/check")
-    public ResponseEntity<String> getTrafficCameras() {
+    public ResponseEntity<String> getTrafficCameras(@RequestParam String latitude, @RequestParam String longitude) {
         try {
-            String cameraData = trafficCameraService.getTrafficCameraData();
+            String cameraData = trafficCameraService.getTrafficCameraData( latitude, longitude);
             return ResponseEntity.ok(cameraData);
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
--- src/main/java/org/dev2ne1/speedalertapi/service/TrafficCameraService.java
+++ src/main/java/org/dev2ne1/speedalertapi/service/TrafficCameraService.java
@@ -21,7 +21,7 @@ public class TrafficCameraService {
     @Value("${api.key}") // application.properties에서 api.key 값을 읽어옵니다.
     private String apiKey;
 
-    public String getTrafficCameraData() throws IOException {
+    public String getTrafficCameraData(String latitude, String longitude) throws IOException {
         StringBuilder urlBuilder = new StringBuilder("http://api.data.go.kr/openapi/tn_pubr_public_unmanned_traffic_camera_api");
         urlBuilder.append("?serviceKey=" + URLEncoder.encode(apiKey, "UTF-8"));
         urlBuilder.append("&pageNo=" + URLEncoder.encode("1", "UTF-8"));
@@ -29,38 +29,38 @@ public class TrafficCameraService {
         urlBuilder.append("&type=" + URLEncoder.encode("xml", "UTF-8"));
 
         // 필요한 추가 파라미터들
-        urlBuilder.append("&mnlssRegltCameraManageNo=" + URLEncoder.encode("", "UTF-8"));
-        urlBuilder.append("&ctprvnNm=" + URLEncoder.encode("", "UTF-8"));
-        urlBuilder.append("&signguNm=" + URLEncoder.encode("", "UTF-8"));
-        urlBuilder.append("&roadKnd=" + URLEncoder.encode("", "UTF-8"));
-        urlBuilder.append("&roadRouteNo=" + URLEncoder.encode("", "UTF-8"));
-        urlBuilder.append("&roadRouteNm=" + URLEncoder.encode("", "UTF-8"));
-        urlBuilder.append("&roadRouteDrc=" + URLEncoder.encode("", "UTF-8"));
-        urlBuilder.append("&rdnmadr=" + URLEncoder.encode("", "UTF-8"));
-        urlBuilder.append("&lnmadr=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&mnlssRegltCameraManageNo=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&ctprvnNm=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&signguNm=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&roadKnd=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&roadRouteNo=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&roadRouteNm=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&roadRouteDrc=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&rdnmadr=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&lnmadr=" + URLEncoder.encode("", "UTF-8"));
         urlBuilder.append("&latitude=" + URLEncoder.encode("36.02879792", "UTF-8"));  /*위도*/
         urlBuilder.append("&longitude=" + URLEncoder.encode("128.5295922", "UTF-8"));  /*경도*/
-        urlBuilder.append("&itlpc=" + URLEncoder.encode("", "UTF-8"));
-        urlBuilder.append("&regltSe=" + URLEncoder.encode("", "UTF-8"));
-        urlBuilder.append("&lmttVe=" + URLEncoder.encode("", "UTF-8"));
-        urlBuilder.append("&regltSctnLcSe=" + URLEncoder.encode("", "UTF-8"));
-        urlBuilder.append("&ovrspdRegltSctnLt=" + URLEncoder.encode("", "UTF-8"));
-        urlBuilder.append("&prtcareaType=" + URLEncoder.encode("", "UTF-8"));
-        urlBuilder.append("&installationYear=" + URLEncoder.encode("", "UTF-8"));
-        urlBuilder.append("&institutionNm=" + URLEncoder.encode("", "UTF-8"));
-        urlBuilder.append("&phoneNumber=" + URLEncoder.encode("", "UTF-8"));
-        urlBuilder.append("&referenceDate=" + URLEncoder.encode("", "UTF-8"));
-        urlBuilder.append("&instt_code=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&itlpc=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&regltSe=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&lmttVe=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&regltSctnLcSe=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&ovrspdRegltSctnLt=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&prtcareaType=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&installationYear=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&institutionNm=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&phoneNumber=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&referenceDate=" + URLEncoder.encode("", "UTF-8"));
+//        urlBuilder.append("&instt_code=" + URLEncoder.encode("", "UTF-8"));
 
         String url = urlBuilder.toString();
-
+        System.out.println("url : "+url);
         URI uri = null; // URI 객체로 변환
         try {
             uri = new URI(url);
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
-
+        System.out.println("uri : "+uri);
         // getForObject를 사용하여 응답을 가져옵니다.
         return restTemplate.getForObject(uri, String.class);
     }
